1547bc8ff9821c1ec66dd886f25904df60afbdaa
Scott Huang <scott@modern-age.com>
2024-05-28 16:32:49 -0500

    Set up Jekyll and Tailwind.css

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+gem "webrick", "~> 1.8"
+
+gem "csv", "~> 3.3"
+
+gem "github-pages", "~> 231", group: :jekyll_plugins
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_site/**/*.html',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './*.html',
+    './**/*.md',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
